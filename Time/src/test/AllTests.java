7465df3bcd44e82aa6c70e03a00d8acbc32ca290
Jing Tian <tian9@email.sc.edu>
2013-11-04 00:19:22 -0800

    Add TimeTest into regression test suite

--- Time/src/test/AllTests.java
+++ Time/src/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MeetingTimeTest.class })
+@SuiteClasses({ MeetingTimeTest.class, TimeTest.class })
 public class AllTests {
 
 }
